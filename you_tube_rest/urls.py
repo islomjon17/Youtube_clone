c066708252ee0f859150cb3b842d8f41ba27a5c3
islomjon17 <turgunovislomjon107@gmail.com>
2022-01-13 10:22:18 +0500

    add urls

--- you_tube_rest/urls.py
+++ you_tube_rest/urls.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-
+from .views import *
 # router = DefaultRouter()
 # router.register('video',PostViewSet)
 
@@ -11,5 +11,9 @@ from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
     # path('',include(router.urls)),
+    path("videolist/", VideoGenricsView.as_view(), name='postlist'),
+    path("commentlist/", CommentGenricsView.as_view(), name='postlist'),
+    path("userlist/", UsersGenricsView.as_view(), name='postlist'),
+    path("playlist/", PlayListGenricsView.as_view(), name='postlist'),
 ]
 
